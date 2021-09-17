edd426145dd093ee0a60e10de3ae695401cb58d4
daviderdner <daviderdner@tamu.edu>
2021-09-17 12:58:14 -0500

    add Procfile and route

---
+++ test_app/spec/unit/unit_spec.rb
@@ -0,0 +1,26 @@
+#location: spec/unit/unit_spec.rb
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+
+    it 'is valid title with valid attributes' do
+        title = Book.new(title: 'Harry Potter')
+        expect(title).to be_valid
+    end
+
+    it 'is valid author with valid attributes' do
+        author = Book.new(author: 'JK Rowling')
+        expect(author).to be_valid
+    end
+
+    it 'is valid price with valid attributes' do
+        price = Book.new(price: 20)
+        expect(price).to be_valid
+    end
+
+    it 'is valid date with valid attributes' do
+        date = Book.new(date: 2000-05-20)
+        expect(date).to be_valid
+    end
+
+end
---
+++ test_app/spec/unit/unit_spec_backup.rb
@@ -0,0 +1,42 @@
+# #location: spec/unit/unit_spec.rb
+# require 'rails_helper'
+
+# RSpec.describe Book, type: :model do
+#     subject do
+#         described_class.new(title: 'harry potter')
+#     end
+
+#     subject do
+#         described_class.new(date: '12/2/1990')
+#     end
+
+#     # subject do
+#     #     described_class.new(author: 'JK Rowling')
+#     # end
+
+#     # subject do
+#     #     described_class.new(price: '20')
+#     # end
+
+#     # subject do
+#     #     described_class.new(date: '12/2/1990')
+#     # end
+
+#     it 'is valid with valid attributes' do
+#         expect(subject).to be_valid
+#     end
+
+#    it 'is not valid without a name' do
+#        subject.title = nil
+#        expect(subject).not_to be_valid
+#    end
+
+#     # it 'is valid date with valid attributes' do
+#     #     expect(subject).to be_valid
+#     # end
+
+#     # it 'is not valid date without a name' do
+#     #     subject.title = nil
+#     #     expect(subject).not_to be_valid
+#     # end
+# end
