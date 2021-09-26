69b3eb147b56c90b67055ddc1841ff4703e691b0
daviderdner <daviderdner@tamu.edu>
2021-09-26 18:06:09 -0500

    finished book assignment by adding delete page

--- test_app/app/controllers/books_controller.rb
+++ test_app/app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
+        format.html { redirect_to books_url, notice: "Book was successfully created." }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
+        format.html { redirect_to books_url, notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -48,12 +48,21 @@ class BooksController < ApplicationController
   end
 
   # DELETE /books/1 or /books/1.json
+  def delete
+    @book=Book.find(params[:id])
+  end
+
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    
+    flash[:notice] = "Book '#{@book.title}' deleted successfully"
+    redirect_to(books_path)
+
+    # @book.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+    # format.json { head :no_content }
   end
 
   private
--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_183346) do
+ActiveRecord::Schema.define(version: 2021_09_02_221605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.float "price"
-    t.string "date"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
